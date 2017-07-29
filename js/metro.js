366ed297eadd6579ec1cddfba2efb62c1aed2fed
Ej <ejan.corp@gmail.com>
2017-07-29 07:11:21 +0400

    update

--- js/metro.js
+++ js/metro.js
@@ -1,151 +1,252 @@
 var Metro = {
-    "red": [{
-        "name": "Rashidiya",
-        "number": "11"
+  "red": [{
+    "name": "Rashidiya",
+    "number": "11"
+  }, {
+    "name": "Emirates",
+    "number": "12"
+  }, {
+    "name": "Airport Terminal 3",
+    "number": "13"
+  }, {
+    "name": "Airport Terminal 1",
+    "number": "14"
+  }, {
+    "name": "GGICO",
+    "number": "15"
+  }, {
+    "name": "Deira City Centre",
+    "number": "16"
+  }, {
+    "name": "Al Rigga",
+    "number": "17"
+  }, {
+    "name": "Union",
+    "number": "18"
+  }, {
+    "name": "BurJuman",
+    "number": "19"
+  }, {
+    "name": "DAMAC (formerly Al Karama)",
+    "number": "20"
+  }, {
+    "name": "Al Jafiliya",
+    "number": "21"
+  }, {
+    "name": "World Trade Centre",
+    "number": "22"
+  }, {
+    "name": "Emirates Towers",
+    "number": "23"
+  }, {
+    "name": "Financial Centre",
+    "number": "24"
+  }, {
+    "name": "Burj Khalifa/Dubai Mall",
+    "number": "25"
+  }, {
+    "name": "Business Bay",
+    "number": "26"
+  }, {
+    "name": "Noor Islamic Bank",
+    "number": "29"
+  }, {
+    "name": "First Gulf Bank",
+    "number": "31"
+  }, {
+    "name": "Mall of the Emirates",
+    "number": "32"
+  }, {
+    "name": "Sharaf DG",
+    "number": "33"
+  }, {
+    "name": "Dubai Internet City",
+    "number": "34"
+  }, {
+    "name": "Nakheel",
+    "number": "35"
+  }, {
+    "name": "DAMAC (formerly Dubai Marina)",
+    "number": "36"
+  }, {
+    "name": "Jumeirah Lakes Towers",
+    "number": "37"
+  }, {
+    "name": "Nakheel Harbour and Tower",
+    "number": "38"
+  }, {
+    "name": "Ibn Battuta",
+    "number": "39"
+  }, {
+    "name": "Energy",
+    "number": "40"
+  }, {
+    "name": "Danube",
+    "number": "41"
+  }, {
+    "name": "UAE Exchange (formerly Jebel Ali)",
+    "number": "42"
+  }],
+  "green": [{
+    "name": "Etisalat",
+    "number": "11"
+  }, {
+    "name": "Al Qusais",
+    "number": "12"
+  }, {
+    "name": "Dubai Airport Free Zone",
+    "number": "13"
+  }, {
+    "name": "Al Nahda",
+    "number": "14"
+  }, {
+    "name": "Stadium",
+    "number": "15"
+  }, {
+    "name": "Al Qiyadah",
+    "number": "16"
+  }, {
+    "name": "Abu Hail",
+    "number": "17"
+  }, {
+    "name": "Abu Baker Al Siddique",
+    "number": "18"
+  }, {
+    "name": "Salah Al Din",
+    "number": "19"
+  }, {
+    "name": "Union",
+    "number": "20"
+  }, {
+    "name": "Baniyas Square",
+    "number": "21"
+  }, {
+    "name": "Palm Deira",
+    "number": "22"
+  }, {
+    "name": "Al Ras",
+    "number": "23"
+  }, {
+    "name": "Al Ghubaiba",
+    "number": "24"
+  }, {
+    "name": "Al Fahidi",
+    "number": "25"
+  }, {
+    "name": "BurJuman",
+    "number": "26"
+  }, {
+    "name": "Oud Metha",
+    "number": "27"
+  }, {
+    "name": "Dubai Healthcare City",
+    "number": "28"
+  }, {
+    "name": "Al Jadaf",
+    "number": "29"
+  }, {
+    "name": "Creek",
+    "number": "30"
+  }]
+}
+
+var MetroDB = {
+  "red": [
+    [{
+      "name": "Rashidiya",
+      "number": "11"
     }, {
-        "name": "Emirates",
-        "number": "12"
+      "name": "Emirates",
+      "number": "12"
     }, {
-        "name": "Airport Terminal 3",
-        "number": "13"
+      "name": "Airport Terminal 3",
+      "number": "13"
     }, {
-        "name": "Airport Terminal 1",
-        "number": "14"
+      "name": "Airport Terminal 1",
+      "number": "14"
     }, {
-        "name": "GGICO",
-        "number": "15"
+      "name": "GGICO",
+      "number": "15"
     }, {
-        "name": "Deira City Centre",
-        "number": "16"
+      "name": "Deira City Centre",
+      "number": "16"
     }, {
-        "name": "Al Rigga",
-        "number": "17"
+      "name": "Al Rigga",
+      "number": "17"
     }, {
-        "name": "Union",
-        "number": "18"
-    }, {
-        "name": "BurJuman",
-        "number": "19"
-    }, {
-        "name": "DAMAC (formerly Al Karama)",
-        "number": "20"
-    }, {
-        "name": "Al Jafiliya",
-        "number": "21"
-    }, {
-        "name": "World Trade Centre",
-        "number": "22"
-    }, {
-        "name": "Emirates Towers",
-        "number": "23"
-    }, {
-        "name": "Financial Centre",
-        "number": "24"
-    }, {
-        "name": "Burj Khalifa/Dubai Mall",
-        "number": "25"
-    }, {
-        "name": "Business Bay",
-        "number": "26"
-    }, {
-        "name": "Noor Islamic Bank",
-        "number": "29"
-    }, {
-        "name": "First Gulf Bank",
-        "number": "31"
-    }, {
-        "name": "Mall of the Emirates",
-        "number": "32"
-    }, {
-        "name": "Sharaf DG",
-        "number": "33"
-    }, {
-        "name": "Dubai Internet City",
-        "number": "34"
-    }, {
-        "name": "Nakheel",
-        "number": "35"
-    }, {
-        "name": "DAMAC (formerly Dubai Marina)",
-        "number": "36"
-    }, {
-        "name": "Jumeirah Lakes Towers",
-        "number": "37"
-    }, {
-        "name": "Nakheel Harbour and Tower",
-        "number": "38"
-    }, {
-        "name": "Ibn Battuta",
-        "number": "39"
-    }, {
-        "name": "Energy",
-        "number": "40"
-    }, {
-        "name": "Danube",
-        "number": "41"
-    }, {
-        "name": "UAE Exchange (formerly Jebel Ali)",
-        "number": "42"
+      "name": "Union",
+      "number": "18"
     }],
-    "green": [{
-        "name": "Etisalat",
-        "number": "11"
+    [{
+      "name": "Union",
+      "number": "18"
     }, {
-        "name": "Al Qusais",
-        "number": "12"
+      "name": "BurJuman",
+      "number": "19"
     }, {
-        "name": "Dubai Airport Free Zone",
-        "number": "13"
+      "name": "DAMAC (formerly Al Karama)",
+      "number": "20"
     }, {
-        "name": "Al Nahda",
-        "number": "14"
+      "name": "Al Jafiliya",
+      "number": "21"
     }, {
-        "name": "Stadium",
-        "number": "15"
+      "name": "World Trade Centre",
+      "number": "22"
     }, {
-        "name": "Al Qiyadah",
-        "number": "16"
+      "name": "Emirates Towers",
+      "number": "23"
     }, {
-        "name": "Abu Hail",
-        "number": "17"
+      "name": "Financial Centre",
+      "number": "24"
     }, {
-        "name": "Abu Baker Al Siddique",
-        "number": "18"
+      "name": "Burj Khalifa/Dubai Mall",
+      "number": "25"
     }, {
-        "name": "Salah Al Din",
-        "number": "19"
+      "name": "Business Bay",
+      "number": "26"
     }, {
-        "name": "Union",
-        "number": "20"
+      "name": "Noor Islamic Bank",
+      "number": "29"
     }, {
-        "name": "Baniyas Square",
-        "number": "21"
+      "name": "First Gulf Bank",
+      "number": "31"
     }, {
-        "name": "Palm Deira",
-        "number": "22"
+      "name": "Mall of the Emirates",
+      "number": "32"
     }, {
-        "name": "Al Ras",
-        "number": "23"
+      "name": "Sharaf DG",
+      "number": "33"
     }, {
-        "name": "Al Ghubaiba",
-        "number": "24"
+      "name": "Dubai Internet City",
+      "number": "34"
     }, {
-        "name": "Al Fahidi",
-        "number": "25"
+      "name": "Nakheel",
+      "number": "35"
     }, {
-        "name": "BurJuman",
-        "number": "26"
+      "name": "DAMAC (formerly Dubai Marina)",
+      "number": "36"
     }, {
-        "name": "Oud Metha",
-        "number": "27"
+      "name": "Jumeirah Lakes Towers",
+      "number": "37"
     }, {
-        "name": "Dubai Healthcare City",
-        "number": "28"
+      "name": "Nakheel Harbour and Tower",
+      "number": "38"
+    }],
+    [{
+      "name": "Nakheel Harbour and Tower",
+      "number": "38"
     }, {
-        "name": "Al Jadaf",
-        "number": "29"
+      "name": "Ibn Battuta",
+      "number": "39"
     }, {
-        "name": "Creek",
-        "number": "30"
+      "name": "Energy",
+      "number": "40"
+    }, {
+      "name": "Danube",
+      "number": "41"
+    }, {
+      "name": "UAE Exchange (formerly Jebel Ali)",
+      "number": "42"
     }]
+  ]
 }
--- js/script.js
+++ js/script.js
@@ -1,174 +1,290 @@
 (function() {
 
-    'use strict'
+  'use strict'
 
-    var Map = function(element, data) {
+  var Map = function(element, data) {
 
-        this.element = element;
-        this.data = data;
-        this.svg = null;
+    this.element = element;
+    this.data = data;
 
-        this.metroRedKey = 'red';
+    this.stationCollection = [];
+    this.svg = null;
 
-        this.height = 120;
-        this.width = 120;
+    this.metroRedKey = 'red';
 
-        this.circle = {
-            xaxis: 60,
-            yaxis: 60,
-            radius: 30,
-            fill: '#fff',
-            stroke: '#000',
-            strokeWidth: 7
-        };
+    this.height = 120;
+    this.width = 120;
 
-        this.line = {
-            fill: '#fff',
-            stroke: '#E10031',
-            strokeWidth: 7
-        }
-
-        this.fill = '#000';
-        this.stroke = '#000';
-        this.strokeWidth = 20;
-        this.fontFamily = '\'Open Sans\', sans-serif';
-        this.fontWeightBold = 'bold';
+    this.circle = {
+      xaxis: 60,
+      yaxis: 60,
+      radius: 30,
+      fill: '#fff',
+      stroke: '#000',
+      strokeWidth: 7
     };
 
-    Map.prototype.init = function() {
-
-        this.svg = d3.select('#map').append('svg')
-            .attr('width', this.width * this.getMetroRed().length)
-            .attr('height', this.height)
-            .selectAll('g')
-            .data(this.getMetroRed())
-            .enter().append('g')
-            .attr('transform', function(d, i) {
-                var xpos = (this.width * this.getMetroRed().length) - ((i + 1) * this.width);
-                document.querySelector(this.element).getElementsByTagName('svg')[0].setAttribute('height', (i + 1) * this.height);
-                return 'translate(' + xpos.toString() + ',' + i * this.height + ')';
-            }.bind(this));
-
-        this.setPreviousLine().setNextLine();
-        this.setCircles().setStationName();
-
-        this.svg.append('g');
-    };
-
-    Map.prototype.setMap = function(mapWidth, mapHeight, mapData) {
-
-    };
-
-    Map.prototype.setCircles = function() {
-        this.svg.append('circle')
-            .attr('cx', this.circle.xaxis)
-            .attr('cy', this.circle.yaxis)
-            .attr('r', this.circle.radius)
-            .style('fill', this.circle.fill)
-            .style('stroke', this.circle.stroke)
-            .style('stroke-width', this.circle.strokeWidth)
-            .style('visibility', function(d) {
-                return d.name ? 'visible' : 'hidden';
-            });
-        return this;
-    };
-
-    Map.prototype.setStationName = function() {
-        this.svg.append('text')
-            .attr('x', 0)
-            .attr('y', 0)
-            .attr('dy', '1em')
-            .attr('font-size', '11px')
-            .attr('font-family', this.fontFamily)
-            .attr('font-weight', this.fontWeightBold)
-            .style('fill', this.fill)
-            .text(function(d) {
-                return d.name;
-            })
-            .style('visibility', function(d) {
-                return d.name ? 'visible' : 'hidden';
-            });
-        return this;
-    };
-
-    Map.prototype.setPreviousLine = function() {
-        this.svg.append('line')
-            .attr('x1', 60)
-            .attr('x2', 120)
-            .attr('y1', 60)
-            .attr('y2', 0)
-            .style('fill', this.line.fill)
-            .attr('stroke', this.line.stroke)
-            .attr('stroke-width', this.line.strokeWidth);
-        return this;
-    };
-
-    Map.prototype.setNextLine = function() {
-        this.svg.append('line')
-            .attr('x1', 60)
-            .attr('x2', 0)
-            .attr('y1', 60)
-            .attr('y2', 120)
-            .style('fill', this.line.fill)
-            .attr('stroke', this.line.stroke)
-            .attr('stroke-width', this.line.strokeWidth);
-        return this;
-    };
-
-    /**
-     * [description]
-     */
-
-    Map.prototype.fillEmptyStations = function(stations) {
-        var range = this.createRange(parseInt(stations[0].number), parseInt(stations.slice(-1)[0].number));
-        return range.map(this.generateMissingStation.bind({
-            stations: stations,
-            numbers: stations.map(this.getStationNumberFromObject)
-        }));
-    };
-
-    Map.prototype.createRange = function(start, end) {
-        return Array
-            .apply(null, Array((end - start) + 1))
-            .map(function(discard, n) {
-                return n + start;
-            });
+    this.line = {
+      fill: '#fff',
+      stroke: '#E10031',
+      strokeWidth: 30
     }
 
-    Map.prototype.sortStations = function(a, b) {
-        return a.number - b.number;
+    this.fill = '#000';
+    this.stroke = '#000';
+    this.strokeWidth = 20;
+    this.fontFamily = '\'Open Sans\', sans-serif';
+    this.fontWeightBold = 'bold';
+
+
+    /** New Vars **/
+
+    this.element = element;
+    this.data = data;
+
+  };
+
+  Map.prototype.initialize = function() {
+
+  };
+
+  /** Prototype **/
+  /******************************************/
+  Map.prototype.init = function() {
+    //this.metroOne();
+    this.metroTwo();
+  };
+
+  Map.prototype.metroOne = function() {
+    this.svg = d3.select('#map').append('svg')
+      .attr('width', this.width * this.getMetroRed().length)
+      .attr('height', this.height)
+      .selectAll('g')
+      .data(this.getMetroRed())
+      .enter().append('g')
+      .attr('transform', this.setElementPosition.bind(this));
+
+    this.setLeftPreviousLine().setLeftNextLine();
+    this.setCircles().setRightStationName();
+
+    this.svg.append('g');
+  }
+
+  Map.prototype.metroTwo = function() {
+    this.svg = d3.select('#map').append('svg')
+      .attr('width', this.width * this.getMetroRed(1).length)
+      .attr('height', this.height)
+      .selectAll('g')
+      .data(this.getMetroRed(1))
+      .enter().append('g')
+      .attr('transform', this.setElementPosition.bind(this));
+
+    this.setLeftPreviousLine().setLeftNextLine();
+    this.setCircles().setRightStationName();
+
+    this.svg.append('g');
+  };
+
+  Map.prototype.setElementPosition = function(d, i, s) {
+    var xpos = (this.width * s.length) - ((i + 1) * this.width);
+    var ypos = (this.height * s.length) - ((i + 1) * this.height);
+
+    //var ypos = i * this.height; // start upwards
+
+    this.stationCollection.push({
+      name: d.name,
+      number: d.number,
+      xpos: xpos,
+      ypos: ypos,
+      seq: i
+    });
+
+    document.querySelector(this.element).getElementsByTagName('svg')[0].setAttribute('height', (i + 1) * this.height);
+    return 'translate(' + xpos + ',' + ypos + ')';
+  };
+
+  Map.prototype.setCircles = function() {
+    this.svg.append('circle')
+      .attr('cx', this.circle.xaxis)
+      .attr('cy', this.circle.yaxis)
+      .attr('r', this.circle.radius)
+      .style('fill', this.circle.fill)
+      .style('stroke', this.circle.stroke)
+      .style('stroke-width', this.circle.strokeWidth)
+      .style('visibility', function(d) {
+        return d.name ? 'visible' : 'hidden';
+      });
+    return this;
+  };
+
+  Map.prototype.setStationName = function() {
+    this.svg.append('text')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('dy', '1em')
+      .attr('font-size', '11px')
+      .attr('font-family', this.fontFamily)
+      .attr('font-weight', this.fontWeightBold)
+      .style('fill', this.fill)
+      .text(function(d) {
+        return d.number;
+      })
+      .style('visibility', function(d) {
+        return d.name ? 'visible' : 'hidden';
+      });
+    return this;
+  };
+
+  Map.prototype.setRightStationName = function() {
+    this.svg.append('text')
+      .attr('x', 60)
+      .attr('y', 0)
+      .attr('dy', '1em')
+      .attr('font-size', '13px')
+      .attr('font-family', this.fontFamily)
+      .attr('font-weight', this.fontWeightBold)
+      .style('fill', this.fill)
+      .text(function(d) {
+        return d.name;
+      })
+      .style('visibility', function(d) {
+        return d.name ? 'visible' : 'hidden';
+      });
+
+    this.svg.append('text')
+      .attr('x', 54)
+      .attr('y', 54)
+      .attr('dy', '1em')
+      .attr('font-size', '11px')
+      .attr('font-family', this.fontFamily)
+      .attr('font-weight', this.fontWeightBold)
+      .style('fill', this.fill)
+      .text(function(d) {
+        return d.number;
+      })
+      .style('visibility', function(d) {
+        return d.name ? 'visible' : 'hidden';
+      });
+    return this;
+  };
+
+  Map.prototype.setCircleNumber = function() {
+
+  };
+
+  Map.prototype.setRightPreviousLine = function() {
+    this.svg.append('line')
+      .attr('x1', 60)
+      .attr('x2', 120)
+      .attr('y1', 60)
+      .attr('y2', 0)
+      .style('fill', this.line.fill)
+      .attr('stroke', this.line.stroke)
+      .attr('stroke-width', this.line.strokeWidth);
+    return this;
+  };
+
+  Map.prototype.setRightNextLine = function() {
+    this.svg.append('line')
+      .attr('x1', 60)
+      .attr('x2', 0)
+      .attr('y1', 60)
+      .attr('y2', 120)
+      .style('fill', this.line.fill)
+      .attr('stroke', this.line.stroke)
+      .attr('stroke-width', this.line.strokeWidth);
+    return this;
+  };
+
+  Map.prototype.setLeftPreviousLine = function() {
+    this.svg.append('line')
+      .attr('x1', 60)
+      .attr('x2', 0)
+      .attr('y1', 60)
+      .attr('y2', 0)
+      .style('fill', this.line.fill)
+      .attr('stroke', this.line.stroke)
+      .attr('stroke-width', this.line.strokeWidth)
+      .style('visibility', function(d, i, s) {
+        return (s.length - 1) != i ? 'visible' : 'hidden';
+      });
+    return this;
+  };
+
+  Map.prototype.setLeftNextLine = function() {
+    this.svg.append('line')
+      .attr('x1', 60)
+      .attr('x2', 120)
+      .attr('y1', 60)
+      .attr('y2', 120)
+      .style('fill', this.line.fill)
+      .attr('stroke', this.line.stroke)
+      .attr('stroke-width', this.line.strokeWidth)
+      .style('visibility', function(d, i, s) {
+        return i ? 'visible' : 'hidden';
+      });
+    return this;
+  };
+
+  /**
+   * [description]
+   */
+
+  Map.prototype.fillEmptyStations = function(stations) {
+    var range = this.createRange(parseInt(stations[0].number), parseInt(stations.slice(-1)[0].number));
+    return range.map(this.generateMissingStation.bind({
+      stations: stations,
+      numbers: stations.map(this.getStationNumberFromObject)
+    }));
+  };
+
+  Map.prototype.createRange = function(start, end) {
+    return Array
+      .apply(null, Array((end - start) + 1))
+      .map(function(discard, n) {
+        return n + start;
+      });
+  }
+
+  Map.prototype.sortStations = function(a, b) {
+    return a.number - b.number;
+  };
+
+  Map.prototype.getFormattedMetro = function(line, part) {
+    part = typeof(part) === 'undefined' ? 0 : part;
+    return this.data[line][part].map(this.getStationDataFromObject);
+  };
+
+  Map.prototype.getMetroRed = function(part) {
+    return this.fillEmptyStations(this.getFormattedMetro(this.metroRedKey, part).sort(this.sortStations));
+  };
+
+  Map.prototype.getStationDataFromObject = function(station) {
+    return {
+      name: station.name,
+      number: parseInt(station.number)
     };
+  };
 
-    Map.prototype.getFormattedMetro = function(line) {
-        return this.data[line].map(this.getStationDataFromObject);
+  Map.prototype.getStationNumberFromObject = function(station) {
+    return parseInt(station.number);
+  };
+
+  Map.prototype.generateMissingStation = function(val, index) {
+    if (this.numbers.indexOf(val) !== -1)
+      return this.stations[this.numbers.indexOf(val)];
+
+    return {
+      name: '',
+      number: val
     };
+  }
 
-    Map.prototype.getMetroRed = function() {
-        return this.fillEmptyStations(this.getFormattedMetro(this.metroRedKey).sort(this.sortStations));
-    };
+  window || (window = {});
+  window.Metro || (window.Metro = {});
+  window.Metro.Map = (window.Metro.Map = {});
 
-    Map.prototype.getStationDataFromObject = function(station) {
-        return {
-            name: station.name,
-            number: parseInt(station.number)
-        };
-    };
-
-    Map.prototype.getStationNumberFromObject = function(station) {
-        return parseInt(station.number);
-    };
-
-    Map.prototype.generateMissingStation = function(val, index) {
-        if (this.numbers.indexOf(val) !== -1)
-            return this.stations[this.numbers.indexOf(val)];
-
-        return {
-            name: '',
-            number: val
-        };
-    }
-
-    var app = new Map('#map', window.Metro);
-    app.init();
+  window.Metro.Map = new Map('#map', window.MetroDB);
+  window.Metro.Map.initialize();
 
 })();
