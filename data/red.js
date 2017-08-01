04df358351221992b31ef07b586663f9e10085b2
Efren Corpuz <ejcorpuz@tradetracker.com>
2017-08-01 13:38:22 +0400

    update js

--- data/red.js
+++ data/red.js
@@ -2,41 +2,65 @@ var Red = [{
     "name": "Rashidiya",
     "number": 11,
     "position": {
-        x: 4500,
-        y: 100
+        x: 3800,
+        y: 300
     },
-    "direction": "left",
+    "direction": "right",
     "curve": "bottom"
 }, {
     "name": "Emirates",
     "number": 12,
     "position": {
-        x: 4400,
-        y: 200
+        x: 3900,
+        y: 400
     },
-    "direction": "left"
+    "direction": "right"
 }, {
     "name": "Airport Terminal 3",
     "number": 13,
     "position": {
-        x: 4300,
-        y: 300
+        x: 4000,
+        y: 500
     },
-    "direction": "left"
+    "direction": "right"
 }, {
     "name": "Airport Terminal 1",
     "number": 14,
     "position": {
-        x: 4200,
-        y: 400
+        x: 4100,
+        y: 600
     },
-    "direction": "left"
+    "direction": "right"
 }, {
     "name": "GGICO",
     "number": 15,
     "position": {
-        x: 4100,
-        y: 500
+        x: 4200,
+        y: 700
     },
-    "direction": "left"
+    "direction": "right"
+}, {
+    "name": "Deira City Centre",
+    "number": "16",
+    "position": {
+        x: 4300,
+        y: 800
+    },
+    "direction": "right"
+}, {
+    "name": "Al Rigga",
+    "number": "17",
+    "position": {
+        x: 4400,
+        y: 900
+    },
+    "direction": "right"
+}, {
+    "name": "Union",
+    "number": "18",
+    "position": {
+        x: 4500,
+        y: 1000
+    },
+    "direction": "right"
 }];
--- js/lib.js
+++ js/lib.js
@@ -19,6 +19,10 @@
         this.lineStrokeWidth = 20;
         this.lineFill = "none";
 
+        this.fontFamily = '\'Open Sans\', sans-serif';
+        this.fontWeightBold = 'bold';
+        this.fontFill = "#000";
+
         this.direction = {
             "left": 0,
             "right": 1,
@@ -38,11 +42,25 @@
 
         _.each(this.dataCells, this.setCellPolyline.bind(this));
         _.each(this.dataCells, this.setCellCircle.bind(this));
+        _.each(this.dataCells, this.setStationNumber.bind(this));
     };
 
     Map.prototype.forEachStation = function(station) {
         station.element = this.grid.svg.select("#" + this.grid.cellClass + String(station.position.x) + String(station.position.y));
         return station;
+    };
+
+    Map.prototype.setStationNumber = function(cell) {
+        cell.element.append("text")
+            .attr('x', this.grid.getObjectXpos)
+            .attr('y', this.grid.getObjectYpos)
+            .attr('dy', '1em')
+            .attr('font-size', '12px')
+            .attr('font-family', this.fontFamily)
+            .attr('font-weight', this.fontWeightBold)
+            .style('fill', this.fontFill)
+            .text(this.getStationNumber.bind(this, cell))
+            .attr('transform', this.getTextTransformTranslate);
     };
 
     Map.prototype.setCellCircle = function(cell) {
@@ -63,6 +81,14 @@
             .style('stroke-width', this.lineStrokeWidth)
             .attr("points", this.getLinePoints.bind(this, cell));
     };
+
+    Map.prototype.getStationNumber = function(cell, data) {
+        return cell.name;
+    };
+
+    Map.prototype.getTextTransformTranslate = function(data) {
+        return "translate(" + (data.width / 2) + "," + "0" + ")";
+    }
 
     Map.prototype.getLinePoints = function(cell, data) {
 
