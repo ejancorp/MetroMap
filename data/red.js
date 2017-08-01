85aac41463d5924afe581635f213a1de2f0ccae4
Efren Corpuz <ejcorpuz@tradetracker.com>
2017-08-01 11:00:58 +0400

    update line to polyline

--- data/red.js
+++ data/red.js
@@ -1,36 +1,41 @@
 var Red = [{
-  "name": "Rashidiya",
-  "number": 11,
-  "position": {
-    x: 4500,
-    y: 100
-  },
+    "name": "Rashidiya",
+    "number": 11,
+    "position": {
+        x: 4500,
+        y: 100
+    },
+    "direction": "left"
 }, {
-  "name": "Emirates",
-  "number": 12,
-  "position": {
-    x: 4400,
-    y: 200
-  },
+    "name": "Emirates",
+    "number": 12,
+    "position": {
+        x: 4400,
+        y: 200
+    },
+    "direction": "left"
 }, {
-  "name": "Airport Terminal 3",
-  "number": 13,
-  "position": {
-    x: 4300,
-    y: 300
-  },
+    "name": "Airport Terminal 3",
+    "number": 13,
+    "position": {
+        x: 4300,
+        y: 300
+    },
+    "direction": "left"
 }, {
-  "name": "Airport Terminal 1",
-  "number": 14,
-  "position": {
-    x: 4200,
-    y: 400
-  },
+    "name": "Airport Terminal 1",
+    "number": 14,
+    "position": {
+        x: 4200,
+        y: 400
+    },
+    "direction": "left"
 }, {
-  "name": "GGICO",
-  "number": 15,
-  "position": {
-    x: 4100,
-    y: 500
-  },
+    "name": "GGICO",
+    "number": 15,
+    "position": {
+        x: 4100,
+        y: 500
+    },
+    "direction": "left"
 }];
--- js/lib.js
+++ js/lib.js
@@ -1,44 +1,101 @@
 (function(d3, _) {
 
-  "use strict";
+    "use strict";
 
-  function Map(data, grid) {
+    function Map(data, grid, color) {
 
-    this.data = data;
-    this.grid = grid;
+        this.data = data;
+        this.grid = grid;
+        this.color = color;
 
-    this.plots = null;
-    this.dataCells = [];
-  }
+        this.plots = null;
+        this.dataCells = [];
 
-  Map.prototype.init = function() {
+        this.circleFill = "#FFF";
+        this.circleStroke = "#000";
+        this.circleStrokeWidth = 10;
+        this.circleRadius = 30;
 
-    this.dataCells = _.map(this.data, this.forEachStation.bind(this));
+        this.direction = {
+            "left": 0,
+            "right": 1,
+        };
+    }
 
-    _.each(this.dataCells, this.setCellCircle.bind(this));
-  };
+    Map.prototype.init = function() {
 
-  Map.prototype.forEachStation = function(station) {
-    station.element = this.grid.svg.select("#" + this.grid.cellClass + String(station.position.x) + String(station.position.y));
-    return station;
-  };
+        this.dataCells = _.map(this.data, this.forEachStation.bind(this));
 
-  Map.prototype.setCellCircle = function(cell) {
-    cell.element
-      .append("circle")
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      })
-      .attr("r", 30)
-      .style("fill", "#000")
-      .style("stroke", "#000")
-      .style("stroke-width", 20)
-  };
+        //_.each(this.dataCells, this.setDiagonalLine.bind(this));
+        _.each(this.dataCells, this.setCellPolyline.bind(this));
+        _.each(this.dataCells, this.setCellCircle.bind(this));
+    };
 
-  var app = new Map(window.Red, window.Map.Grid);
-  app.init();
+    Map.prototype.forEachStation = function(station) {
+        station.element = this.grid.svg.select("#" + this.grid.cellClass + String(station.position.x) + String(station.position.y));
+        return station;
+    };
+
+    Map.prototype.setCellCircle = function(cell) {
+        cell.element.append("circle")
+            .attr("cx", this.grid.getObjectXpos)
+            .attr("cy", this.grid.getObjectYpos)
+            .attr("r", this.circleRadius)
+            .style("fill", this.circleFill)
+            .style("stroke", this.circleStroke)
+            .style("stroke-width", this.circleStrokeWidth)
+            .attr("transform", this.getCircleTranslateCenter);
+    };
+
+    Map.prototype.setDiagonalLine = function(cell) {
+        cell.element.append("line")
+            .attr('x1', this.getLineXone.bind(this, cell))
+            .attr('x2', this.getLineXtwo.bind(this, cell))
+            .attr('y1', this.getLineYone.bind(this, cell))
+            .attr('y2', this.getLineYtwo.bind(this, cell))
+            .style('stroke', this.color)
+            .style('stroke-width', 10)
+    };
+
+    Map.prototype.setCellPolyline = function(cell) {
+        cell.element.append("polyline")
+            .style("stroke", "black")
+            .style("fill", "none")
+            .style("stroke-width", 10)
+            .attr("points", this.getLinePoints.bind(this, cell));
+    };
+
+    Map.prototype.getLinePoints = function(cell, data) {
+        var x = [data.x, data.y + data.height];
+        var y = [data.x + data.width, data.y];
+        return x.join(", ") + " " + y.join(", ");
+    }
+
+    Map.prototype.getLineYone = function(cell, data) {
+        if (!this.direction[cell.direction])
+            return data.y + data.height;
+        return data.y;
+    };
+
+    Map.prototype.getLineYtwo = function(cell, data) {
+        if (!this.direction[cell.direction])
+            return data.y;
+        return data.y + data.height;
+    };
+
+    Map.prototype.getLineXone = function(cell, data) {
+        return data.x;
+    };
+
+    Map.prototype.getLineXtwo = function(cell, data) {
+        return data.x + data.width;
+    };
+
+    Map.prototype.getCircleTranslateCenter = function(data) {
+        return "translate(" + data.width / 2 + "," + data.height / 2 + ")";
+    };
+
+    var app = new Map(window.Red, window.Map.Grid, "#EA4335");
+    app.init();
 
 })(d3, _);
