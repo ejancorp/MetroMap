0358a2ad70bd3f0ecdb9c908b8c90e76a1f01bdf
Efren Corpuz <ejcorpuz@tradetracker.com>
2017-08-02 09:14:13 +0400

    update metro map

--- data/green.js
+++ data/green.js
@@ -1,106 +1,192 @@
 var Green = [{
-  "name": "Etisalat",
-  "number": "11",
-  "position": {
-    "x": 4100,
-    "y": 1200
-  },
-  "direction": "right"
-}, {
-  "name": "Al Qusais",
-  "number": "12",
-  "position": {
-    "x": 4000,
-    "y": 1100
-  },
-  "direction": "right"
-}, {
-  "name": "Dubai Airport Free Zone",
-  "number": "13",
-  "position": {
-    "x": 3900,
-    "y": 1000
-  },
-  "direction": "right"
-}, {
-  "name": "Al Nahda",
-  "number": "14",
-  "position": {
-    "x": 3800,
-    "y": 900
-  },
-  "direction": "right"
-}, {
-  "name": "Stadium",
-  "number": "15",
-  "position": {
-    "x": 3700,
-    "y": 800
-  },
-  "direction": "right"
-}, {
-  "name": "Al Qiyadah",
-  "number": "16",
-  "position": {
-    "x": 3600,
-    "y": 700
-  },
-  "direction": "right"
-}, {
-  "name": "Abu Hail",
-  "number": "17",
-  "position": {
-    "x": 3500,
-    "y": 600
-  },
-  "direction": "left",
-  "curve": "bottom"
-}, {
-  "name": "Abu Baker Al Siddique",
-  "number": "18",
-  "position": {
-    "x": 3400,
-    "y": 700
-  },
-  "direction": "left"
-}, {
-  "name": "Salah Al Din",
-  "number": "19",
-  "position": {
-    "x": 3300,
-    "y": 800
-  },
-  "direction": "left"
-}, {
-  "name": "Baniyas Square",
-  "number": "21",
-  "position": {
-    "x": 3000,
-    "y": 800
-  },
-  "direction": "left"
-}, {
-  "name": "Palm Deira",
-  "number": "22",
-  "position": {
-    "x": 3000,
-    "y": 700
-  },
-  "direction": "left"
-}, {
-  "name": "Al Ras",
-  "number": "23",
-  "position": {
-    "x": 3000,
-    "y": 700
-  },
-  "direction": "left"
-}, {
-  "name": "Al Ghubaiba",
-  "number": "24",
-  "position": {
-    "x": 3100,
-    "y": 800
-  },
-  "direction": "left"
-}];
+        "name": "Etisalat",
+        "number": "11",
+        "position": {
+            "x": 4100,
+            "y": 1200
+        },
+        "direction": "right"
+    }, {
+        "name": "Al Qusais",
+        "number": "12",
+        "position": {
+            "x": 4000,
+            "y": 1100
+        },
+        "direction": "right"
+    }, {
+        "name": "Dubai Airport Free Zone",
+        "number": "13",
+        "position": {
+            "x": 3900,
+            "y": 1000
+        },
+        "direction": "right"
+    }, {
+        "name": "Al Nahda",
+        "number": "14",
+        "position": {
+            "x": 3800,
+            "y": 900
+        },
+        "direction": "right"
+    }, {
+        "name": "Stadium",
+        "number": "15",
+        "position": {
+            "x": 3700,
+            "y": 800
+        },
+        "direction": "right"
+    }, {
+        "name": "Al Qiyadah",
+        "number": "16",
+        "position": {
+            "x": 3600,
+            "y": 700
+        },
+        "direction": "right"
+    }, {
+        "name": "Abu Hail",
+        "number": "17",
+        "position": {
+            "x": 3500,
+            "y": 600
+        },
+        "direction": "left",
+        "curve": "bottom"
+    }, {
+        "name": "Abu Baker Al Siddique",
+        "number": "18",
+        "position": {
+            "x": 3400,
+            "y": 700
+        },
+        "direction": "left"
+    }, {
+        "name": "Salah Al Din",
+        "number": "19",
+        "position": {
+            "x": 3300,
+            "y": 800
+        },
+        "direction": "left"
+    }, {
+        "name": "Union",
+        "number": "18",
+        "position": {
+            "x": 3200,
+            "y": 900
+        },
+        "direction": "left",
+        "curve": "top"
+    }, {
+        "name": "",
+        "number": "",
+        "position": {
+            "x": 3100,
+            "y": 800
+        },
+        "direction": "right"
+    }, {
+        "name": "Baniyas Square",
+        "number": "21",
+        "position": {
+            "x": 3000,
+            "y": 700
+        },
+        "direction": "right"
+    }, {
+        "name": "",
+        "number": "",
+        "position": {
+            "x": 2900,
+            "y": 600
+        },
+        "direction": "right",
+        "curve": "bottom"
+    }, {
+        "name": "Palm Deira",
+        "number": "22",
+        "position": {
+            "x": 2800,
+            "y": 700
+        },
+        "direction": "left"
+    }, {
+        "name": "Al Ras",
+        "number": "23",
+        "position": {
+            "x": 2700,
+            "y": 800
+        },
+        "direction": "left"
+    }, {
+        "name": "",
+        "number": "",
+        "position": {
+            "x": 2600,
+            "y": 900
+        },
+        "direction": "right",
+        "curve": "right"
+    }, {
+        "name": "Al Ghubaiba",
+        "number": "24",
+        "position": {
+            "x": 2700,
+            "y": 1000
+        },
+        "direction": "right"
+    }, {
+        "name": "Al Fahidi",
+        "number": "25",
+        "position": {
+            "x": 2800,
+            "y": 1100
+        },
+        "direction": "right"
+    },
+    {
+        "name": "BurJuman",
+        "number": "19",
+        "position": {
+            "x": 2900,
+            "y": 1200
+        },
+        "direction": "right"
+    },
+    {
+        "name": "Oud Metha",
+        "number": "27",
+        "position": {
+            "x": 3000,
+            "y": 1300
+        },
+        "direction": "right"
+    }, {
+        "name": "Dubai Healthcare City",
+        "number": "28",
+        "position": {
+            "x": 3100,
+            "y": 1400
+        },
+        "direction": "right"
+    }, {
+        "name": "Al Jadaf",
+        "number": "29",
+        "position": {
+            "x": 3200,
+            "y": 1500
+        },
+        "direction": "right"
+    }, {
+        "name": "Creek",
+        "number": "30",
+        "position": {
+            "x": 3300,
+            "y": 1600
+        },
+        "direction": "right"
+    }
+];
