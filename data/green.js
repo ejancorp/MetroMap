639b997efcca57e82bd1436ee0c4bb9f38bb7b63
Ej <ejan.corp@gmail.com>
2017-08-02 07:41:28 +0400

    update

--- data/green.js
+++ data/green.js
@@ -2,73 +2,105 @@ var Green = [{
   "name": "Etisalat",
   "number": "11",
   "position": {
-    x: 4700,
-    y: 600
+    "x": 4100,
+    "y": 1200
   },
   "direction": "right"
 }, {
   "name": "Al Qusais",
   "number": "12",
   "position": {
-    x: 4600,
-    y: 500
+    "x": 4000,
+    "y": 1100
   },
   "direction": "right"
 }, {
   "name": "Dubai Airport Free Zone",
   "number": "13",
   "position": {
-    x: 4500,
-    y: 400
+    "x": 3900,
+    "y": 1000
   },
   "direction": "right"
 }, {
   "name": "Al Nahda",
   "number": "14",
   "position": {
-    x: 4400,
-    y: 300
+    "x": 3800,
+    "y": 900
   },
   "direction": "right"
 }, {
   "name": "Stadium",
   "number": "15",
   "position": {
-    x: 4300,
-    y: 200
+    "x": 3700,
+    "y": 800
   },
   "direction": "right"
 }, {
   "name": "Al Qiyadah",
   "number": "16",
   "position": {
-    x: 4200,
-    y: 100
+    "x": 3600,
+    "y": 700
   },
   "direction": "right"
 }, {
   "name": "Abu Hail",
   "number": "17",
   "position": {
-    x: 4100,
-    y: 00
+    "x": 3500,
+    "y": 600
   },
   "direction": "left",
-  "curve": "bottom",
+  "curve": "bottom"
 }, {
   "name": "Abu Baker Al Siddique",
   "number": "18",
   "position": {
-    x: 4000,
-    y: 100
+    "x": 3400,
+    "y": 700
   },
-  "direction": "left",
+  "direction": "left"
 }, {
   "name": "Salah Al Din",
   "number": "19",
   "position": {
-    x: 3900,
-    y: 200
+    "x": 3300,
+    "y": 800
   },
-  "direction": "left",
+  "direction": "left"
+}, {
+  "name": "Baniyas Square",
+  "number": "21",
+  "position": {
+    "x": 3000,
+    "y": 800
+  },
+  "direction": "left"
+}, {
+  "name": "Palm Deira",
+  "number": "22",
+  "position": {
+    "x": 3000,
+    "y": 700
+  },
+  "direction": "left"
+}, {
+  "name": "Al Ras",
+  "number": "23",
+  "position": {
+    "x": 3000,
+    "y": 700
+  },
+  "direction": "left"
+}, {
+  "name": "Al Ghubaiba",
+  "number": "24",
+  "position": {
+    "x": 3100,
+    "y": 800
+  },
+  "direction": "left"
 }];
